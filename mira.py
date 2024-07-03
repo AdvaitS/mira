6d2310d3a0091ee055c280ae8adc60bad034d3e4
Advait <advait.shukla10@gmail.com>
2024-07-03 15:55:55 -0400

    updates

--- mira.py
+++ mira.py
@@ -1,16 +1,23 @@
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-import time, os, datetime, json, threading
+import time, os, datetime, json, threading, boto3
 from peripherals import Motor, Temperature, Camera
 from gpiozero import OutputDevice, LED
-from routines import Experiment
+from awsiot import mqtt5_client_builder
+from awscrt import mqtt5, http
+from concurrent.futures import Future
+import cv2
+
+future_stopped = Future()
+future_connection_success = Future()
+received_count = 0
 
 class Incubator():
 	def __init__(self):
 		self.mqtt_client = self.establish_mqtt()
 		self.dtFormat = '%Y-%m-%d %H:%M:%S'
-		self.name = os.getenv("Incubator")
+		self.name = os.environ["INCUBATOR"]
 		self.led = LED(18)
-		self.motor = Motor([21,20,16,7])
+		self.motor = Motor([12,16,20,21])
 		self.heater = OutputDevice(13)
 		self.fan = OutputDevice(23)
 		self.camera = Camera()
@@ -18,64 +25,140 @@ class Incubator():
 		self.experiment = None
 		self.stop_temp_event = threading.Event()
 		self.stop_temp_event.set()
-		threading.Thread(target=self.temperature_stream, daemon=True).start()
+		
+		#threading.Thread(target=self.status_message, daemon=True).start()
 	
 	def establish_mqtt(self):
-		mqtt_client = AWSIoTMQTTClient(os.getenv("INCUBATOR"))
-		mqtt_client.configureEndpoint(os.getenv("ENDPOINT"), 8883)
-		mqtt_client.configureCredentials(os.getenv("ROOTCA"), os.getenv("PRIVKEY"), os.getenv("CERTIF"))
+		client = mqtt5_client_builder.mtls_from_path(
+        endpoint=os.getenv("ENDPOINT"),
+        port=8883,
+        cert_filepath=os.getenv("CERTIF"),
+        pri_key_filepath=os.getenv("PRIVKEY"),
+        ca_filepath=os.getenv("ROOTCA"),
+        on_publish_received=self.on_publish_received,
+        on_lifecycle_stopped=self.on_lifecycle_stopped,
+        on_lifecycle_connection_success=self.on_lifecycle_connection_success,
+        on_lifecycle_connection_failure=self.on_lifecycle_connection_failure,
+        client_id=os.getenv("INCUBATOR"))
+		client.start()
+		sub_future = client.subscribe(subscribe_packet=mqtt5.SubscribePacket(subscriptions=[mqtt5.Subscription(topic_filter="request/led",qos=mqtt5.QoS.AT_LEAST_ONCE), 
+																							mqtt5.Subscription(topic_filter="request/motor",qos=mqtt5.QoS.AT_LEAST_ONCE),
+																							mqtt5.Subscription(topic_filter="request/temperature",qos=mqtt5.QoS.AT_LEAST_ONCE),
+																							mqtt5.Subscription(topic_filter="request/status",qos=mqtt5.QoS.AT_LEAST_ONCE),
+																							mqtt5.Subscription(topic_filter="request/experiment",qos=mqtt5.QoS.AT_LEAST_ONCE)
+																							]))
+		#mqtt_client.subscribe("request/led", 1, self.led_message)
+		#mqtt_client.subscribe("request/motor", 1, self.motor_message)
+		#mqtt_client.subscribe("request/image", 1, self.image_message)
+		#mqtt_client.subscribe("request/status", 1, self.status_message)
+		#mqtt_client.subscribe("request/experiment", 1, self.experiment_message)
+		suback = sub_future.result(100)
+		return client
+	
+	# Callback when any publish is received
+	def on_publish_received(self, publish_packet_data):
+		publish_packet = publish_packet_data.publish_packet
+		assert isinstance(publish_packet, mqtt5.PublishPacket)
+		print("Received message from topic'{}':{}".format(publish_packet.topic, publish_packet.payload))
+		global received_count
+		received_count += 1
+		if publish_packet.topic == "request/led":
+			self.led_message(publish_packet.payload)
+			
+		elif publish_packet.topic == "request/motor":
+			self.motor_message(publish_packet.payload)
+			
+		elif publish_packet.topic == "request/image":
+			self.image_message(publish_packet.payload)
+			
+		elif publish_packet.topic == "request/status":
+			self.status_message(publish_packet.payload)
+			
+		elif publish_packet.topic == "request/experiment":
+			self.experiment_message(publish_packet.payload)
 
-		mqtt_client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-		mqtt_client.configureDrainingFrequency(2)  # Draining: 2 Hz
-		mqtt_client.configureConnectDisconnectTimeout(10)  # 10 sec
-		mqtt_client.configureMQTTOperationTimeout(5)  # 5 sec
+	# Callback for the lifecycle event Stopped
+	def on_lifecycle_stopped(self, lifecycle_stopped_data: mqtt5.LifecycleStoppedData):
+		print("Lifecycle Stopped")
+		global future_stopped
+		future_stopped.set_result(lifecycle_stopped_data)
 
-		mqtt_client.connect()
 
-		mqtt_client.subscribe("request/led", 1, self.led_message)
-		mqtt_client.subscribe("request/motor", 1, self.motor_message)
-		mqtt_client.subscribe("request/image", 1, self.image_message)
-		mqtt_client.subscribe("request/status", 1, self.status_message)
-		mqtt_client.subscribe("request/experiment", 1, self.experiment_message)
-		
-		return mqtt_client
-		
-	def led_message(self, client, userdata, message):
-		print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Received message: {message.payload}")
-		command = message.payload.decode()
-		if command == 'on':
+	# Callback for the lifecycle event Connection Success
+	def on_lifecycle_connection_success(self, lifecycle_connect_success_data: mqtt5.LifecycleConnectSuccessData):
+		print("Lifecycle Connection Success")
+		global future_connection_success
+		future_connection_success.set_result(lifecycle_connect_success_data)
+
+
+	# Callback for the lifecycle event Connection Failure
+	def on_lifecycle_connection_failure(self, lifecycle_connection_failure: mqtt5.LifecycleConnectFailureData):
+		print("Lifecycle Connection Failure")
+		print("Connection failed with exception:{}".format(lifecycle_connection_failure.exception))
+    
+	def led_message(self, payload):
+		#print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Received message: {payload}")
+		command = payload.decode()
+		print(command)
+		if command == "on":
 			self.led.on()
-		elif command == 'off':
+		elif command == "off":
 			self.led.off()
 	
-	def motor_message(self, client, userdata, message):
-		print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Received message: {message.payload}")
-		command = message.payload.decode()
+	def motor_message(self, payload):
+		#print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Received message: {payload}")
+		command = payload.decode()
 		if command == 'clockwise':
 			self.motor.rotate(60)
 		elif command == 'anticlockwise':
 			self.motor.rotate(60, clockwise=False)
     
-	def image_message(self, client, userdata, message):
-		print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Received message: {message.payload}")
-		payload = self.camera.snapshot()		#TODO
-		self.mqtt_client.publish("response/image", json.dumps(payload), 1)
-	
-	def status_message(self, client, userdata, message):
-		print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Received message: {message.payload}")
+	def image_message(self, payload):
+		#print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Received message: {message.payload}")
+		image = self.camera.take_snapshot()		#TODO
+		try:
+			filename = "snap.jpg"#f"{self.name}_{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.jpg"
+			key = os.path.join(self.aws_folder, filename)
+			image_string = cv2.imencode('.jpg', image)[1].tobytes()
+			self.s3.put_object(Bucket=self.bucket_name, Key = key, Body=image_string)
+			s3_url = f"https://{self.bucket_name}.s3.amazonaws.com/{key}"
+			presigned_url = self.generate_presigned_url(key)
+			payload = {"status": "1", "image_url": presigned_url}
+			print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Sending message: {payload}")
+			self.mqtt_client.publish(mqtt5.PublishPacket(topic="response/image", payload=json.dumps(payload), qos=mqtt5.QoS.AT_LEAST_ONCE))
+		except Exception as e:
+			print(e)
+			payload = {"status": "0", "image_url": "None"}
+			print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Sending message: {payload}")
+			self.mqtt_client.publish(mqtt5.PublishPacket(topic="response/image", payload=json.dumps(payload), qos=mqtt5.QoS.AT_LEAST_ONCE))
+			
+		#self.mqtt_client.publish(mqtt5.PublishPacket(topic="response/image", payload=json.dumps(payload), qos=mqtt5.QoS.AT_LEAST_ONCE))
+	def generate_presigned_url(self, object_name, expiration=3600):
+		response = self.s3.generate_presigned_url('get_object', Params={'Bucket':self.bucket_name, 'Key': object_name}, ExpiresIn=expiration)
+		return response
+		
+	def status_message(self, payload):
+		#print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Received message: {message.payload}")
+		#while self.stop_temp_event.is_set():
 		temp_payload = self.temperature.capture()
-		work_payload = 
+		if self.experiment is not None:
+			work_payload = self.experiment.status
+			payload = {**work_payload, **temp_payload}
+		else:
+			payload = temp_payload
 		print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Sending message: {payload}")
-		self.mqtt_client.publish("response/status", json.dumps(payload), 1)
+		self.mqtt_client.publish(mqtt5.PublishPacket(topic="response/status", payload=json.dumps(payload), qos=mqtt5.QoS.AT_LEAST_ONCE))
+		#self.mqtt_client.publish("response/status", json.dumps(payload), 1)
+		#time.sleep(3)
 	
-	def experiment_message(self, client, userdata, message):
-		print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Received message: {message.payload}")
-		#command = message.payload.decode()
-		if message.payload["command"] == 'start':
+	def experiment_message(self, payload):
+		#print(f"[{datetime.datetime.now().strftime(self.dtFormat)}]		Received message: {message.payload}")
+		command = json.loads(payload.decode())
+		if command['command'] == 'start':
 			if self.experiment is None:
-				Experiment(self, message.payload["device_name"], message.payload["duration"], message.payload["interval"], message.payload["exp_file"], message.payload["samples"])
+				Experiment(self, command["device_name"], command["duration"], command["interval"], command["experiment_file"], {1:command["sample_1"],2:command["sample_2"], 3:command["sample_3"], 4:command["sample_4"], 5:command["sample_5"], 6:command["sample_6"]})
 			else:
-				break
+				return
 		elif command == 'stop':
 			self.experiment.stop_experiment()
 		
@@ -100,7 +183,101 @@ class Incubator():
 	
 	def update_shadow():
 		pass
+
+class Experiment(Incubator):
+	def __init__(self, incubator, device_name, duration, interval, exp_file, samples):
+		#super().__init__()
+		self.incubator = incubator
+		self.incubator.experiment = self
+		self.device_name = device_name
+		self.duration = float(duration)
+		self.interval = int(interval)
+		self.exp_file = exp_file
+		self.samples = samples
+		self.status = {'experiment_running': 'False', 
+						'device_name': f"{self.device_name}", 
+						'duration':f"{self.duration}", 
+						'interval':f"{self.interval}", 
+						'exp_file':f"{self.exp_file}", 
+						'sample_names':f"{self.samples}", 
+						'iterations': '0'}
+		self.local_dir = self.make_unique_dir(self.status['exp_file'])
+		self.start_experiment_thread = threading.Thread(target=self.start_experiment, daemon=True)
+		self.experiment_flag = threading.Event()
+		self.start_time = datetime.datetime.now()
+		self.end_time = self.start_time + datetime.timedelta(hours=self.duration)
+		self.start_experiment_thread.start()
+		self.status["start_time"] = self.start_time.strftime('%Y-%m-%d %H:%M:%S')
+		
+	def make_unique_dir(self, base):
+		unique_dir = None
+		if not os.path.exists(base):
+			os.makedirs(base)
+			unique_dir = base
+		else:
+			subscript=1
+			unique_dir = f"{base}_{subscript}"
+			while os.path.exists(unique_dir):
+				subscript += 1
+				unique_dir = f"{base}_{subscript}"
+			os.makedirs(unique_dir)
+		for dish, name in self.samples.items():
+			os.makedirs(os.path.join(unique_dir, f"{dish}-{name}"))
+		return unique_dir
+		
+		
+	def start_experiment(self):
+		
+		self.status["experiment_running"] = "True"
+		self.experiment_flag.set()
+		
+		while self.experiment_flag.is_set():
+			self.status["iterations"] = str(int(self.status["iterations"])+1)
+			if not self.incubator.motor.isAtHome():
+				self.status["task"] = "Moving to Plate 1"
+				self.incubator.motor.turn(self.incubator.motor.home_plate, 1)
+				self.incubator.motor.turnThread.join()
+			
+			self.run_iteration()
+			
+			if datetime.datetime.now() > end_time:
+				break
+			else:
+				self.status["task"] = "Waiting for next iteration"
+				time.sleep(self.interval * 60)
+			
+		self.stop_experiment()
+
+	def run_iteration(self):
+		
+		for dish in range(1, 7):
+				self.incubator.led.on()
+				self.status["task"] = "Taking Image"
+				image = self.incubator.camera.take_snapshot()
+				self.incubator.led.off()
 				
+				self.status["task"] = "Sending Image"
+				image_name = f"{self.incubator.name}_{dish}_{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.jpg"
+				exp_image_path = os.path.join(self.local_dir, f"{dish}-{self.samples[dish]}", image_name)
+				self.incubator.camera.send_image(image, exp_image_path)
+				
+				self.status["task"] = f"Moving to Plate {(dish%6)+1}"
+				self.incubator.motor.turn(self.incubator.motor.next_plate, 1)
+				self.incubator.motor.turnThread.join()
+		
+		
+	def stop_experiment(self):
+		self.experiment_flag.clear()
+		self.status["experiment_running"] = "False"
+		self.status["task"] = "Idle"
+		self.led.off()
+		self.heater.off()
+		time.sleep(60)
+		self.fan.off()
+		self.incubator.experiment = None
+		del self
+		
+
 if __name__ == "__main__":
 	session = Incubator()
 	
--- peripherals.py
+++ peripherals.py
@@ -1,7 +1,10 @@
-import subprocess, threading, datetime
+import subprocess, threading, datetime, time, base64
 import board
 import busio
+from simple_pid import PID
 from adafruit_ahtx0 import AHTx0
+from gpiozero import OutputDevice
+import cv2, boto3, os
 
 class Motor():
     def __init__(self, pins):
@@ -17,6 +20,9 @@ class Motor():
             [0, 0, 1, 1],
             [0, 0, 0, 1]
 	]
+        self.turnThread = None
+        self.ps1 = None
+        self.ps2 = None
         
     def rotate(self, degrees, clockwise=True):
         #rotates the motor
@@ -30,21 +36,52 @@ class Motor():
                 for pin, state in zip(self.pins, step):
                     pin.value = state
                 time.sleep(0.005)
-                
+    
+    def turn(self, function, direction):
+        self.turnThread = threading.Thread(target=function, args=(direction,), daemon=True)
+        self.turnThread.start()
+    
+    def home_plate(self, direction):
+        self.rotate(60, direction)
+    
+    def next_plate(self, direction):
+        self.rotate(60, direction)
+        
+    def isAtHome(self):
+        return False
+    
+    def isAtPlate(self):
+        return True
+        
 class Camera():
     def __init__(self, shutter = "1000"):
         self.camera_lock = threading.Lock()
-        self.snap_filename = "/tmp/image.jpg"
+        self.image_path = "/tmp/image.jpg"
         self.shutter_time = shutter
+        self.dtFormat = '%Y-%m-%d %H:%M:%S'
+        self.s3 = boto3.client('s3')
+        self.bucket_name = 'petriboothproject'
+        self.aws_exp_folder = os.path.join(os.environ["BUCKETNAME"], 'Experiments')
 	
     def take_snapshot(self):
-        subprocess.run(["libcamera-still", "-o", self.snap_filename,"--shutter", self.shutter_time])
+        subprocess.run(["libcamera-still", "-o", self.image_path,"--shutter", self.shutter_time])
+        image = cv2.imread(self.image_path) 
+        return image
     
-    def startCameraStream(self):
-        pass
-		
-    def stopCameraStream(self):
-        pass
+    def send_image(self, image, filename):
+        try:
+            key = os.path.join(self.aws_exp_folder, filename)
+            image_string = cv2.imencode('.jpg', image)[1].tobytes()
+            self.s3.put_object(Bucket=self.bucket_name, Key = key, Body=image_string)
+            #s3_url = f"https://{self.bucket_name}.s3.amazonaws.com/{key}"
+            #presigned_url = self.generate_presigned_url(key)
+            #payload = {"status": "1", "image_url": presigned_url}
+
+        except Exception as e:
+            print(e)
+            print(f"Saving Image {self.image_path} at {filename}")
+            os.system(f"cp {self.image_path} {filename}")
+        
     
 
 class Temperature():
@@ -53,6 +90,14 @@ class Temperature():
         self.sensor_address = sensor_address
         self.channel = channel
         self.dtFormat = '%Y-%m-%d %H:%M:%S'
+        self.setpoint = None
+        self.pid_thread = threading.Thread(target=self.pid, daemon=True)
+        self.fan = OutputDevice()
+        self.heater = OutputDevice()
+        self._pause_event = threading.Event()
+        self._pause_event.set()
+        self.pid_thread_event = threading.Event()
+        self.pid_thread_event.set()
 	
     def capture(self):
         try:
@@ -63,7 +108,29 @@ class Temperature():
                 humidity = round(sensor.relative_humidity,2)
                 return {"success": "1", "timestamp": f"{datetime.datetime.now().strftime(self.dtFormat)}", "temperature": f"{temperature}", "humidity": f"{humidity}"}
         except Exception as e:
-            return {"success": "0", "error": f"{e}"}
+            return {"success": "0", "error": f"{e}", "temperature": None, "humidity": None}
+            
+    def pid(self):
+        print("[INFO] Setting the temperature to "+str(setpoint))
+        pid = PID(1,0.5,0,float(self.setpoint))                                             #sets P,I,D values
+        pid.output_limits = (0,204)                                                    #defines output values                               
+        self.fan.on()
+        while self.pid_thread_event.is_set():
+            self._pause_event.wait()
+            temp = self.capture()["temperature"]
+            
+            if temp is None:
+                continue
+            else:
+                temp = float(temp)
+
+            duty_cycle = pid(temp)
+            if(setpoint > temp):
+                self.gpio.heater.value =duty_cycle/255                                 #send the pid generated value to heater
+            else: 
+                self.gpio.heater.value = 0.01                                          #if temperature overshoots/ if the set temperature is achieved it will set the heater to fixed value to maintain that temperature 
+            time.sleep(1)
+        
 
 
 	    
